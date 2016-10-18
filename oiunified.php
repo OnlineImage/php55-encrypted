de815e1928fac26c17a2c6bcac273b08102a88cc
Mike Langone <langone@onlineimage.com>
2016-10-18 13:31:50 -0600

    Change Version number
    Changed Version Number

--- oiunified.php
+++ oiunified.php
@@ -1,13 +1,14 @@
 <?php
 /*
   Plugin Name: OI Unified
-  Version: 2.0.1
+  Version: 2.0.2
   Plugin URI: https://www.onlineimage.com
   Author: The Dev Team
   Author URI: https://www.onlineimage.com/
   Description: Plugin that requires for activation
  */
  // 2.0.1 Added millcreek ut to zipcode database 
+ // 2.0.2 Version update check 
  if( ! class_exists( 'OIUnified_Updater' ) ){
 	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
 }
