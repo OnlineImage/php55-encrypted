d798d3720b39ed5cf24ac37af8cbe7c2c3f17a20
Mike Langone <langone@onlineimage.com>
2016-10-18 13:19:48 -0600

    Begin Updater code
    Added a updated and GitHub check

--- oiunified.php
+++ oiunified.php
@@ -8,6 +8,17 @@
   Description: Plugin that requires for activation
  */
  // 2.0.1 Added millcreek ut to zipcode database 
+ if( ! class_exists( 'OIUnified_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+$updater = new OIUnified_Updater( __FILE__ );
+$updater->set_username( 'OnlineImage' );
+$updater->set_repository( 'php55-encrypted' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+*/
+$updater->initialize(); 
  // Block direct requests
 if ( !defined('ABSPATH') )
     die('-1');
